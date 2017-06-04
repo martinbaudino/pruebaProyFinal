ec7a10321c23f752c27f6b6c75fba7569f030a4c
Martín Baudino <martinbaudino@gmail.com>
2017-06-04 20:27:26 -0300

    Agregados archivos de proyecto final del curso

---
+++ proyFinal/inc/proyFin.h
@@ -0,0 +1,129 @@
+/* Copyright 2016, 6ta Escuela RUSE
+ * All rights reserved.
+ *
+ * This file is part of CIAA Firmware.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef ACT8_H
+#define ACT8_H
+/** \brief Encabezado de Aplicación de Actividad 7
+ **
+ ** This is a mini example of the CIAA Firmware
+ **
+ **/
+
+/** \addtogroup CIAA_Firmware CIAA Firmware
+ ** @{ */
+/** \addtogroup Examples CIAA Firmware Examples
+ ** @{ */
+/** \addtogroup Baremetal Bare Metal example header file
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ *
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * 20160602 v0.0.1 initials initial version
+ */
+
+/*==================[inclusions]=============================================*/
+#include "stdint.h"
+
+/*==================[macros]=================================================*/
+#define lpc4337            1
+#define mk60fx512vlq15     2
+
+/*==================[typedef]================================================*/
+
+/*==================[external data declaration]==============================*/
+#if (CPU == mk60fx512vlq15)
+/* Reset_Handler is defined in startup_MK60F15.S_CPP */
+void Reset_Handler( void );
+
+extern uint32_t __StackTop;
+#elif (CPU == lpc4337)
+/** \brief Reset ISR
+ **
+ ** ResetISR is defined in cr_startup_lpc43xx.c
+ **
+ ** \remark the definition is in
+ **         externals/drivers/cortexM4/lpc43xx/src/cr_startup_lpc43xx.c
+ **/
+extern void ResetISR(void);
+
+/** \brief Stack Top address
+ **
+ ** External declaration for the pointer to the stack top from the Linker Script
+ **
+ ** \remark only a declaration is needed, there is no definition, the address
+ **         is set in the linker script:
+ **         externals/base/cortexM4/lpc43xx/linker/ciaa_lpc4337.ld.
+ **/
+extern void _vStackTop(void);
+
+
+void RIT_IRQHandler(void);
+
+uint8_t ciclaleds(uint8_t comando);
+
+#else
+#endif
+
+/*==================[external functions declaration]=========================*/
+
+/* Manejador de Interrupciones del Timer RIT */
+void ISR_RIT_Handler(void);
+
+/* Inicializador de los temporizadores por soft*/
+void sft_tmr_init(void);
+
+void leds_procesar(void);
+void opciones_procesar(uint8_t comando);
+void sierra_procesar(void);
+
+
+void teclas_procesar(void);
+
+void sft_tim_init(void);
+
+void sft_tim_procesar(void);
+
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef ACT7_H */
+
---
+++ proyFinal/src/proyFin.c
@@ -0,0 +1,142 @@
+/* Copyright 2016, 6ta Escuela RUSE
+ * All rights reserved.
+ *
+ * This file is part of CIAA Firmware.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/** \addtogroup CIAA_Firmware CIAA Firmware
+ ** @{ */
+
+/** \addtogroup Examples CIAA Firmware Examples
+ ** @{ */
+/** \addtogroup Baremetal Bare Metal example source file
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ *
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * 20160602 v0.0.1 initials initial version
+ */
+
+/*==================[inclusions]=============================================*/
+#include "proyFin.h"       /* <= own header */
+#include "uart.h"
+#include "led.h"
+#include "adc.h"
+
+/*==================[internal data declaration]==============================*/
+
+
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+/** \brief Main function
+ *
+ * This is the main entry point of the software.
+ *
+ * \returns 0
+ *
+ * \remarks This function never returns. Return value is only to avoid compiler
+ *          warnings or errors.
+ */
+
+
+
+int main(void)
+{
+	uint8_t valorADC_H, valorADC_L;
+	uint16_t valorADC;
+	uint32_t i;
+	float fVal;
+
+	adc_init(1);
+	InitUart(UART2, 9600);
+	leds_init();
+
+	// adc_convertir();
+	// valorADC=adc_pool(1);
+
+	while(1){
+		adc_convertir();
+		valorADC=adc_pool(1);
+
+		fVal=valorADC;
+		fVal=(fVal*3.3)/1023;
+
+		SendUartFloatAscii(UART2, fVal, 2);
+		WriteUartByte(UART2, '\r');
+
+
+		//valorADC_L=(uint8_t)(valorADC&0x00FF);
+		//valorADC_H=(uint8_t)((valorADC>>8)&0x00FF);
+
+		//WriteUartByte(UART2, valorADC_L);
+		//WriteUartByte(UART2, valorADC_H);
+
+
+		for(i=0; i<600000; i++);
+	}
+
+}
+
+
+void ISR_RIT_Handler(void)
+{
+
+}
+
+void teclas_procesar(void)
+{
+
+}
+
+void leds_procesar(void)
+{
+
+}
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+
